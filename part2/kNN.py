ecf04b55383608c4e81db6beb511f65530e7a4fb
niushuai <guisu2010@gmail.com>
2015-04-19 13:03:32 +0800

    原始的datingTestSet.txt在执行代码时会报错，我对数据起来了修改

--- part2/kNN.py
+++ part2/kNN.py
@@ -2,6 +2,64 @@ from numpy import *
 import operator
 
 def createDataSet():
-    group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+    group = array([[1.0,1.1],[1.0,1.0],[0,2],[0,0.1]])
     labels = ['A','A','B','B']
     return group, labels
+
+
+def classify0(inX,dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = tile(inX, (dataSetSize,1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()
+    classCount={}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+    sortedClassCount = sorted(classCount.iteritems(),
+                              key=operator.itemgetter(1),reverse=True)
+    return sortedClassCount[0][0]
+
+# 2-2
+def file2matrix(filename):
+    fr = open(filename)
+    numberOfLines = len(fr.readlines())         #get the number of lines in the file
+    returnMat = zeros((numberOfLines,3))        #prepare matrix to return
+    classLabelVector = []                       #prepare labels return   
+    fr = open(filename)
+    index = 0
+    for line in fr.readlines():
+        line = line.strip()
+        listFromLine = line.split('\t')
+        returnMat[index,:] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    return returnMat,classLabelVector
+    
+'''
+
+
+excute:
+
+reload(kNN)
+datingDataMat,datingLabels = kNN.file2matrix('datingTestSet.txt')
+
+and 
+
+import matplotlib
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(datingDataMat[:,1],datingDataMat[:,2])
+plt.show()
+
+and
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.scatter(datingDataMat[:,1],datingDataMat[:,2],15.0*array(datingLabels),15.0*array(datingLabels))
+plt.show()
+
+'''
